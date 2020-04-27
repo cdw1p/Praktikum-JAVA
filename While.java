4b5be02284061ddc7d6e5b6364735134b083bfc6
Cahyo Dwi Putro <17512940+cdw1p@users.noreply.github.com>
2020-04-27 21:04:30 +0700

    Create While.java

---
+++ While.java
@@ -0,0 +1,15 @@
+package pkgwhile;
+
+/**
+ *
+ * @author 1337
+ */
+public class While {
+    public static void main(String[] args) {
+        int count=1;
+        while(count<11) {
+            System.out.println("Count adalah "+count);
+            count++;
+        }
+    }
+}
