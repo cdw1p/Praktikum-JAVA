e5bb7600abd1f25bb4e65847577cc0e39332425a
Cahyo Dwi Putro <17512940+cdw1p@users.noreply.github.com>
2020-04-27 21:27:59 +0700

    Create EnhancedForArray.java

---
+++ EnhancedForArray.java
@@ -0,0 +1,14 @@
+package enhancedforarray;
+
+/**
+ *
+ * @author 1337
+ */
+public class EnhancedForArray {
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+        for (int item : numbers) {
+            System.out.println("Angka adalah "+item);
+        }
+    }
+}
