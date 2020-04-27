699c80e37c10f8b0480d48b48e8b94fd71fc0abd
Cahyo Dwi Putro <17512940+cdw1p@users.noreply.github.com>
2020-04-27 21:06:20 +0700

    Create DoWhile.java

---
+++ DoWhile.java
@@ -0,0 +1,15 @@
+package dowhile;
+
+/**
+ *
+ * @author 1337
+ */
+public class DoWhile {
+    public static void main(String[] args) {
+        int count=1;
+        do {
+            System.out.println("Count adalah "+count);
+            count++;
+        } while (count<=11);
+    }
+}
