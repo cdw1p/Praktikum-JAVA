036871268dbf48c9fbe94ec4302d0cc25d9d5d04
Cahyo Dwi Putro <17512940+cdw1p@users.noreply.github.com>
2020-04-27 21:23:15 +0700

    Create LoopForAccessArray.java

---
+++ LoopForAccessArray.java
@@ -0,0 +1,16 @@
+package loopforaccessarray;
+
+/**
+ *
+ * @author 1337
+ */
+public class LoopForAccessArray {
+    public static void main(String[] args) {
+        int[] arrayOfInt = {32, 3, 89, 900, 1082, 20, 30, 127};
+        
+        for (int i=0; i<arrayOfInt.length; i++) {
+            System.out.print(arrayOfInt[i]+" ");
+        }
+        System.out.println();
+    }
+}
