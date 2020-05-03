297df6d3486fbb0a0b8aee02694a8bb17b030d46
Cahyo Dwi Putro <17512940+cdw1p@users.noreply.github.com>
2020-05-03 14:27:42 +0700

    Create ModifierAccessProtected.java

---
+++ ModifierAccessProtected.java
@@ -0,0 +1,13 @@
+package modifier;
+
+public class Person {
+    protected String name;
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+}
