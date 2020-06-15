f65fd628ad95b48b1881c727b78ff107091881d2
Cahyo Dwi Putro <17512940+cdw1p@users.noreply.github.com>
2020-06-15 11:03:04 +0700

    Create Bapak.java

---
+++ ContohInheritance2/Bapak.java
@@ -0,0 +1,15 @@
+package contohinheritance2;
+
+/**
+ *
+ * @author CahyoDwi
+ */
+class Bapak {
+    int a;
+    int b;
+    
+    void show_variabel() {
+        System.out.println("Nilai a = " +a);
+        System.out.println("Nilai b = " +b);
+    }
+}
