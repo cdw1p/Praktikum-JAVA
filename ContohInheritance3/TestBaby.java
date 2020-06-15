f44bf95f2cbe13a460bcb980d4d2950b9a124f34
Cahyo Dwi Putro <17512940+cdw1p@users.noreply.github.com>
2020-06-15 11:32:28 +0700

    Create TestBaby.java

---
+++ ContohInheritance3/TestBaby.java
@@ -0,0 +1,12 @@
+package contohinheritance3;
+
+/**
+ *
+ * @author CahyoDwi
+ */
+class TestBaby {
+    public static void main(String args[]) {
+        Baby x = new Baby("Raikkonen");
+        x.Cry();
+    }
+}
