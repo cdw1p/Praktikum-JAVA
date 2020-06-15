46e90cf889b150d0618522a350e785d19570b176
Cahyo Dwi Putro <17512940+cdw1p@users.noreply.github.com>
2020-06-15 10:31:11 +0700

    Create A.java

---
+++ ContohInheritance1/A.java
@@ -0,0 +1,16 @@
+package contohinheritance;
+
+/**
+ *
+ * @author CahyoDwi
+ */
+class A {
+   String var_a = "Variabel A";
+   String var_b = "Variabel B";
+   String var_c = "Variabel C";
+   String var_d = "Variabel D";
+   
+   A() {
+       System.out.println("Konstruktor A Dijlankan");
+   }
+}
