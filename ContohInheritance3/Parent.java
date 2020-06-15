47dff761f86766fadec7ee045956cf762575fd3d
Cahyo Dwi Putro <17512940+cdw1p@users.noreply.github.com>
2020-06-15 11:32:10 +0700

    Create Parent.java

---
+++ ContohInheritance3/Parent.java
@@ -0,0 +1,14 @@
+package contohinheritance3;
+
+/**
+ *
+ * @author CahyoDwi
+ */
+public class Parent {
+    String parentName;
+    
+    public Parent(String parentName) {
+        this.parentName = parentName;
+        System.out.println("Konstruktor Parent");
+    }
+}
