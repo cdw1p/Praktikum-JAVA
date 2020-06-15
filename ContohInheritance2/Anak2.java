f09aaa62578c73170249a9249a64c81c17f13a6d
Cahyo Dwi Putro <17512940+cdw1p@users.noreply.github.com>
2020-06-15 11:24:23 +0700

    Create Anak2.java

---
+++ ContohInheritance2/Anak2.java
@@ -0,0 +1,16 @@
+package contohinheritance2;
+
+/**
+ *
+ * @author CahyoDwi
+ */
+class Anak extends Bapak {
+    int c;
+    
+    public Anak(int a, int b, int c) {
+        super();
+        System.out.println("Nilai a = " +a);
+        System.out.println("Nilai b = " +b);
+        System.out.println("Nilai c = " +c);
+    }
+}
